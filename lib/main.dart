288a69fad02a624fe3288432f1b7d1b0021a8f73
Danilo Rodrgiues <mydanilows@gmail.com>
2021-09-10 16:03:29 -0300

    new commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,45 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      MaterialApp(
-        home: Scaffold(
-          body: ListTransition(),
-          appBar: AppBar(
-            title: Text('Teste'),
-          ),
-          floatingActionButton: FloatingActionButton(
-            child: Icon(Icons.add),
-            onPressed: () {
-              // Add your onPressed code here!
-            },
-          ),
-        ),
-      ),
+void main() => runApp(ByteBankApp());
+
+class FormTransition extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Novo Teste')),
+      body: Container(),
     );
+  }
+}
+
+class ByteBankApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(body: FormTransition()),
+    );
+  }
+}
 
 class ListTransition extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(children: <Widget>[
-      ItemTransition(Transition(100.0, 1000)),
-      ItemTransition(Transition(300.0, 1001)),
-      ItemTransition(Transition(200.0, 1002)),
-    ]);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Teste'),
+      ),
+      body: Column(children: <Widget>[
+        ItemTransition(Transition(100.0, 1000)),
+        ItemTransition(Transition(300.0, 1001)),
+        ItemTransition(Transition(200.0, 1002)),
+      ]),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          // Add your onPressed code here!
+        },
+      ),
+    );
   }
 }
 
